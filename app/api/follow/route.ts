b7b2a95c099c82a21c040c69a4801db1091a4f29
maheshsp1809 <maheshsp1809@gmail.com>
2024-04-15 17:43:23 +0530

    follow api  page added

---
+++ app/api/follow/route.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export const PUT = async (req: any) => {
+  const body = await req.json();
+  try {
+    await prisma.users.update({
+      where: {
+        id: body.followedById,
+      },
+      data: {
+        following: {
+          connect: {
+            id: body.followedToId,
+          },
+        },
+      },
+    });
+
+    await prisma.users.update({
+      where: {
+        id: body.followedToId,
+      },
+      data: {
+        followers: {
+          connect: {
+            id: body.followedById,
+          },
+        },
+      },
+    });
+    return new NextResponse(JSON.stringify("followed"));
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(JSON.stringify(error), { status: 403 });
+  }
+};
