29965391898e7d275f8e18a1968b5ee64059a605
maheshsp1809 <maheshsp1809@gmail.com>
2024-04-15 15:41:55 +0530

    services added

---
+++ lib/pusher.ts
@@ -0,0 +1,22 @@
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
+
+export const pusherServer = new PusherServer({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_APP_KEY!,
+  secret: process.env.PUSHER_APP_SECRET!,
+  cluster: "ap2",
+  useTLS: true,
+});
+
+
+/**
+ * The following pusher client uses auth, not neccessary for the video chatroom example
+ * Only the cluster would be important for that
+ * These values can be found after creating a pusher app under
+ * @see https://dashboard.pusher.com/apps/<YOUR_APP_ID>/keys
+ */
+
+export const pusherClient = new PusherClient(process.env.PUSHER_APP_KEY!, {
+  cluster: "ap2",
+});
